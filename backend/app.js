286fcbb7f66ef5bf0023d93f86f9bbdbfceb524b
Sofyanne Badir <s.badir@outlook.com>
2022-04-28 16:04:40 +0200

    :recycle: Refactoring du module multer

--- backend/app.js
+++ backend/app.js
@@ -3,48 +3,19 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const multer = require("multer");
+const multer = require("./middleware/multer");
 
 const globalRouter = require("./routes/globalRouter");
 const postRouter = require("./routes/postRouter");
 const userRouter = require("./routes/userRouter");
 
-
-
-
 const app = express();
 
-// Ici je configure ou les fichiers seront stockés et sous quel nom.
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
-  },
-});
-
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jpeg"
-  ) {
-    console.log("Bon format");
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
 app.use(express.json());
-app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
-);
+app.use(multer);
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(morgan("dev")); // Log serveur détaillés.
 
-// Ici j'autorise d'autres domaines à requeter avec différentes méthodes, ainsi qu'à paramétrer les headers.
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -52,7 +23,7 @@ app.use((req, res, next) => {
     "GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next(); // Je demande à express de passer au middleware suivant.
+  next(); 
 });
 
 
---
+++ backend/middleware/multer.js
@@ -0,0 +1,29 @@
+const multer = require("multer");
+
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    console.log("Bon format");
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+module.exports = multer({
+  storage: fileStorage,
+  fileFilter: fileFilter,
+}).single("image");
