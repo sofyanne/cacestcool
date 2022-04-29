e0034cab840266d9eedd6e879147ed025c6b391d
Sofyanne Badir <s.badir@outlook.com>
2022-04-29 14:03:24 +0200

    :adhesive_bandage: Résolution de l'oublie de champs email

--- backend/models/User.js
+++ backend/models/User.js
@@ -11,6 +11,11 @@ const userSchema = Schema(
       type: String,
       require: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
     avatar: {
       type: String,
       require: true,
