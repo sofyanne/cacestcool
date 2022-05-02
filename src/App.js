93642f774c6ea01278c3b36982cf14453dd69eee
Sofyanne Badir <s.badir@outlook.com>
2022-05-02 22:54:43 +0200

    :coffin: Suppression de code inutilisé

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import SignupPage from "./components/Pages/SignupPage";
 import LoginPage from "./components/Pages/LoginPage";
-import Navigation from "./components/Navigation/NavigationComponent"
 
 function App() {
   return (
     <>
+
     <Routes>
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/" element={<LoginPage />} />
